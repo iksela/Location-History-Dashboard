cd6cf675fabb7ab68c6f361d0903df89526fbd1e
Alexandre Mathieu <iksela@gmail.com>
2014-02-16 17:41:14 +0100

    added explore summary

--- data.php
+++ data.php
@@ -8,20 +8,37 @@ if ($_POST['day']) {
 	$data = array();
 	$summary = $db->getSummaryByDay($_POST['day']);
 
+	$html = '';
+
 	foreach ($summary as $event) {
+		$html .= '<div class="well well-sm">';
+
 		$from = new DateTime();
 		$from->setTimestamp($event->dp_from/1000);
 		$to = new DateTime();
 		$to->setTimestamp($event->dp_to/1000);
 		$interval = $from->diff($to);
-		$data[] = array(
+
+		$distance = round($event->distance/1000);
+
+		$html .= "[".$from->format("Y-m-d H:i")." to ".$to->format("Y-m-d H:i")." (".$interval->format("%H:%I").")] ";
+
+		if ($event->moving) {
+			$html .= "Moved $distance km.";
+		}
+		else {
+			$html .= "Idle.";
+		}
+
+		$html .= '</div>';
+/*		$data[] = array(
 			'from'	=> $from->format("Y-m-d H:i"),
 			'to'	=> $to->format("Y-m-d H:i"),
 			'interval'	=> $interval->format("%H:%I"),
 			'moving'	=> $event->moving,
 			'distance'	=> round($event->distance/1000)
-		);
+		);*/
 	}
 	
-	echo json_encode($data);
+	echo $html;
 }
